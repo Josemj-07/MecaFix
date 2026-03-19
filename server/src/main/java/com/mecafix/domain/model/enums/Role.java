3438487d93f981e708c331a8be108efbd37f9bc3
Jose <jose.munozj07@gmail.com>
2026-03-19 14:20:25 -0500

    feat: add Role enum with displayName and helper method

---
+++ server/src/main/java/com/mecafix/domain/model/enums/Role.java
@@ -0,0 +1,18 @@
+package com.mecafix.domain.model.enums;
+
+public enum Role { ADMINISTRATOR("Administrator"), OWNER("Owner");
+
+    private final String displayName;
+
+    Role(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getDisplayName() {
+        return this.displayName;
+    }
+    public boolean isAdministrator() {
+        return this == ADMINISTRATOR;
+    }
+
+}
