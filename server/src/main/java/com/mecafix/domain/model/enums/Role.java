790a51e54057394ab721f0a93a7aa7fda2464c90
Josemj-07 <jose.munozj07@gmail.com>
2026-03-19 14:22:25 -0500

    Update Role.java

--- server/src/main/java/com/mecafix/domain/model/enums/Role.java
+++ server/src/main/java/com/mecafix/domain/model/enums/Role.java
@@ -1,6 +1,8 @@
 package com.mecafix.domain.model.enums;
 
-public enum Role { ADMINISTRATOR("Administrator"), OWNER("Owner");
+public enum Role {
+    
+    ADMINISTRATOR("Administrator"), OWNER("Owner");
 
     private final String displayName;
 
