44a999ae732a2d3924af72400737eb8a38bef86e
carlosandresfv2007 <carlosandresflorezvargas2007@gmail.com>
2026-03-21 15:18:39 -0500

    feat: add Person, Customer, Mechanic, Vehicle and Specialty entities, also their respective exception

---
+++ server/src/main/java/com/mecafix/shared/exceptions/InvalidMechanicException.java
@@ -0,0 +1,7 @@
+package com.mecafix.shared.exceptions;
+
+public class InvalidMechanicException extends RuntimeException {
+    public InvalidMechanicException(String message) {
+        super(message);
+    }
+}
---
+++ server/src/main/java/com/mecafix/shared/exceptions/InvalidPersonException.java
@@ -0,0 +1,7 @@
+package com.mecafix.shared.exceptions;
+
+public class InvalidPersonException extends RuntimeException {
+    public InvalidPersonException(String message) {
+        super(message);
+    }
+}
