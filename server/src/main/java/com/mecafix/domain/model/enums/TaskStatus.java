bb1f77678a9740068b14fa4a60ad5839306ab342
carl <carlitosandresfv@gmail.com>
2026-03-22 17:41:38 -0500

    feat: add Task entity with state machine and TaskStatus enum

---
+++ server/src/main/java/com/mecafix/domain/model/enums/TaskStatus.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package com.mecafix.domain.model.enums;
+
+/**
+ *
+ * @author mpaula
+ */
+public enum TaskStatus {
+    
+}
